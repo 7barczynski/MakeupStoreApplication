1d980846c12a6abe23160e6570453a0a91aadf77
7omasz8 <7barczynski@gmail.com>
2019-11-15 19:03:28 +0100

    Init project

---
+++ src/main/java/com/tbar/MakeupStoreApplication/MakeupStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tbar.MakeupStoreApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeupStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeupStoreApplication.class, args);
+	}
+
+}
