39509abc5834a1ae90eb0157a8ea5216ec9320c2
7omasz8 <7barczynski@gmail.com>
2020-02-02 08:36:29 +0100

    Delete irrelevant code, add proper mappings in main-menu and add new footer to all pages

--- src/main/java/com/tbar/MakeupStoreApplication/utility/AppMappings.java
+++ src/main/java/com/tbar/MakeupStoreApplication/utility/AppMappings.java
@@ -7,6 +7,14 @@ public final class AppMappings {
     public static final String ABOUT = "about";
     public static final String SHOP = "shop";
     public static final String QUERY_PARAM_PAGE = "page";
+    public static final String QUERY_PARAM_PRODUCT_TYPE = "product_type";
+    public static final String QUERY_PARAM_PRODUCT_CATEGORY = "product_category";
+    public static final String QUERY_PARAM_BRAND = "brand";
+    public static final String QUERY_PARAM_PRODUCT_TAGS = "product_tags";
+    public static final String QUERY_PARAM_PRICE_GREATER_THAN = "price_greater_than";
+    public static final String QUERY_PARAM_PRICE_LESS_THAN = "price_less_than";
+    public static final String QUERY_PARAM_RATING_GREATER_THAN = "rating_greater_than";
+    public static final String QUERY_PARAM_RATING_LESS_THAN = "rating_less_than";
 
     // === constructor ===
     private AppMappings() {}
